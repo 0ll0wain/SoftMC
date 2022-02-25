2fee0d687dd4d20aa61f74df7ebcca2d4805cb67
Kim Leidolf <kim.leidolf@gmx.de>
2022-02-25 16:42:32 +0100

    Experimenting in Folder Test

--- sw/Test/test.cpp
+++ sw/Test/test.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <cmath>
 #include "softmc.h"
+#include <unistd.h>
 
 using namespace std;
 
@@ -106,22 +107,31 @@ void readAndCompareRow(fpga_t *fpga, const uint row, const uint bank,
   iseq->execute(fpga);
 
   // Receive the data
-  uint rbuf[16];
+  unsigned int rbuf[16];
   for (int i = 0; i < NUM_COLS;
        i += 8)
-  { // we receive a single burst at two times (32 bytes each)
+  {
+
     fpga_recv(fpga, 0, (void *)rbuf, 16, 0);
-
-    // compare with the pattern
-    uint8_t *rbuf8 = (uint8_t *)rbuf;
-
-    for (int j = 0; j < 64; j++)
+    for (size_t i = 0; i < 16; i++)
     {
-      if (rbuf8[j] != pattern)
-        fprintf(stderr, "Error at Col: %d, Row: %u, Bank: %u, DATA: %x \n", i,
-                row, bank, rbuf8[j]);
+      printf("%x\n", rbuf[i]);
     }
   }
+  // for (int i = 0; i < NUM_COLS;
+  //      i += 8)
+  // { // we receive a single burst at two times (32 bytes each)
+
+  //   uint rbuf[16];
+  //   fpga_recv(fpga, 0, (void *)rbuf, 16, 0);
+  //   // compare with the pattern
+  //   uint8_t *rbuf8 = (uint8_t *)rbuf;
+
+  //   for (int j = 0; j < 64; j++)
+  //   {
+  //     printf("%x\n", rbuf8[j]);
+  //   }
+  // }
 }
 
 void turnBus(fpga_t *fpga, BUSDIR b, InstructionSequence *iseq = nullptr)
@@ -149,94 +159,38 @@ void turnBus(fpga_t *fpga, BUSDIR b, InstructionSequence *iseq = nullptr)
   \param \e fpga is a pointer to the RIFFA FPGA device.
   \param \e retention is the retention time in milliseconds to test.
 */
-void testRetention(fpga_t *fpga, const int retention)
+void test(fpga_t *fpga)
 {
 
-  uint8_t pattern = 0xff; // the data pattern that we write to the DRAM
-
-  bool dimm_done = false;
+  uint8_t pattern = 0x66; // the data pattern that we write to the DRAM
 
   InstructionSequence *iseq = nullptr; // we temporarily store (before sending
                                        // them to the FPGA) the generated
                                        // instructions here
 
-  GET_TIME_INIT(2);
+  uint cur_row = 0;
+  uint cur_bank = 0;
+  // printf("Start Read FIFO flushing\n");
+  // unsigned int rbuf[16];
+  // for (int i = 0; i < 100;
+  //      i++)
+  // {
+  //   fpga_recv(fpga, 0, (void *)rbuf, 16, 20);
+  // }
 
-  // writing the entire row takes approximately 5 ms
-  uint group_size = ceil(
-      retention / 5.0f); // number of rows to be written in a single iteration
-  uint cur_row_write = 0;
-  uint cur_bank_write = 0;
-  uint cur_row_read = 0;
-  uint cur_bank_read = 0;
+  // printf("Read FIFo flushed\n");
+  turnBus(fpga, BUSDIR::WRITE, iseq);
 
-  printf("\n");
+  writeRow(fpga, cur_row, cur_bank, pattern, iseq);
 
-  while (!dimm_done)
-  { // continue until we cover the entire DRAM
-    // print the number of the row that we are about to test
-    printf("%c[2K\r", 27);
-    printf("Current Bank: %d, Row: %d", cur_bank_write, cur_row_write);
-    fflush(stdout);
+  // Switch the memory bus to read mode
+  turnBus(fpga, BUSDIR::READ, iseq);
 
-    // Switch the memory bus to write mode
-    turnBus(fpga, BUSDIR::WRITE, iseq);
+  sleep(1);
 
-    GET_TIME_VAL(0);
+  // Read the data back and compare
 
-    // We write to a chunk of rows (group_size) successively
-    for (int i = 0; i < group_size; i++)
-    {
-      // write the data pattern to the entire row
-      writeRow(fpga, cur_row_write, cur_bank_write, pattern, iseq);
-      cur_row_write++;
-
-      // when we complete testing all of the rows in a bank, we move to the next
-      // bank
-      if (cur_row_write == NUM_ROWS)
-      {
-        cur_row_write = 0;
-        cur_bank_write++;
-      }
-
-      // the entire DIMM is covered when we complete testing all of the bank
-      if (cur_bank_write == NUM_BANKS)
-      {
-        // we are done with the entire DIMM
-        dimm_done = true;
-        break;
-      }
-    }
-
-    // Switch the memory bus to read mode
-    turnBus(fpga, BUSDIR::READ, iseq);
-
-    // wait for the specified retention time (retention)
-    do
-    {
-      GET_TIME_VAL(1);
-    } while ((TIME_VAL_TO_MS(1) - TIME_VAL_TO_MS(0)) < retention);
-
-    // Read the data back and compare
-    for (int i = 0; i < group_size; i++)
-    {
-      readAndCompareRow(fpga, cur_row_read, cur_bank_read, pattern, iseq);
-
-      cur_row_read++;
-
-      if (cur_row_read == NUM_ROWS)
-      { // NUM_ROWS
-        cur_row_read = 0;
-        cur_bank_read++;
-      }
-
-      if (cur_bank_read == NUM_BANKS)
-      { // NUM_BANKS
-        // we are done with the entire DIMM
-        break;
-      }
-    }
-  }
+  readAndCompareRow(fpga, cur_row, cur_bank, pattern, iseq);
 
   printf("\n");
 
@@ -261,48 +215,12 @@ void setRefreshConfig(fpga_t *fpga, uint trefi, uint trfc)
   delete iseq;
 }
 
-void printHelp(char *argv[])
-{
-  cout << "A sample application that tests retention time of DRAM cells using "
-          "SoftMC"
-       << endl;
-  cout << "Usage:" << argv[0] << " [REFRESH INTERVAL]" << endl;
-  cout << "The Refresh Interval should be a positive integer, indicating the "
-          "target retention time in milliseconds."
-       << endl;
-}
-
 int main(int argc, char *argv[])
 {
   fpga_t *fpga;
   fpga_info_list info;
   int fid = 0; // fpga id
   int ch = 0;  // channel id
-
-  if (argc != 2 || strcmp(argv[1], "--help") == 0)
-  {
-    printHelp(argv);
-    return -2;
-  }
-
-  string s_ref(argv[1]);
-  int refresh_interval = 0;
-
-  try
-  {
-    refresh_interval = stoi(s_ref);
-  }
-  catch (...)
-  {
-    printHelp(argv);
-    return -3;
-  }
-
-  if (refresh_interval <= 0)
-  {
-    printHelp(argv);
-    return -4;
-  }
 
   // Get the list of FPGA's attached to the system
   if (fpga_list(&info) != 0)
@@ -339,11 +257,8 @@ int main(int argc, char *argv[])
   // printf("Activating AutoRefresh. tREFI: %d, tRFC: %d \n", trefi, trfc);
   // setRefreshConfig(fpga, trefi, trfc);
 
-  printf("Starting Retention Time Test @ %d ms! \n", refresh_interval);
+  test(fpga);
 
-  testRetention(fpga, refresh_interval);
-
-  printf("The test has been completed! \n");
   fpga_close(fpga);
 
   return 0;
